d9b527402bf50b2c5ee913595f0ee0d2630cf9f6
Brickman <brickman@outlook.in>
2021-02-07 01:51:28 +0800

    bd face

---
+++ baidu/face_search.go
@@ -0,0 +1,63 @@
+package baidu
+
+import (
+	"encoding/base64"
+	"github.com/brickman-source/golang-utilities/http"
+	"github.com/brickman-source/golang-utilities/json"
+	"github.com/brickman-source/golang-utilities/log"
+	"net/url"
+	"strings"
+)
+
+type FaceSearchRequest struct {
+	Image           string `json:"image"`
+	ImageType       string `json:"image_type"`
+	GroupIDList     string `json:"group_id_list"`
+	QualityControl  string `json:"quality_control"`
+	LivenessControl string `json:"liveness_control"`
+}
+
+type FaceSearchResponse struct {
+	FaceToken string `json:"face_token"`
+	UserList  []struct {
+		GroupID  string  `json:"group_id"`
+		UserID   string  `json:"user_id"`
+		UserInfo string  `json:"user_info"`
+		Score    float64 `json:"score"`
+	} `json:"user_list"`
+}
+
+func (bd *Baidu) FaceSearch(userGroupIdList []string, imageData []byte, appId, appSecret string) (*FaceSearchResponse, error) {
+	accessToken, err := bd.GetAccessTokenByClient(appId, appSecret)
+	if err != nil {
+		log.Errorf("cannot get access token(%v): %v", appId, err.Error())
+		return nil, err
+	}
+	bdReqURL, _ := url.Parse(`https://aip.baidubce.com/rest/2.0/face/v3/search`)
+	bdReqQuery := bdReqURL.Query()
+	bdReqQuery.Set("access_token", accessToken.AccessToken)
+	bdReqURL.RawQuery = bdReqQuery.Encode()
+
+	req := &FaceSearchRequest{
+		Image:       base64.StdEncoding.EncodeToString(imageData),
+		ImageType:   "BASE64",
+		GroupIDList: strings.Join(userGroupIdList, ","),
+	}
+
+	bdRespData, err := http.PostData(bdReqURL.String(),
+		http.MIMEApplicationJSONCharsetUTF8,
+		json.ShouldMarshal(req),
+	)
+
+	if err != nil {
+		log.Infof("bd err: %v", err)
+		return nil, err
+	}
+	bdResp := &FaceSearchResponse{}
+	err = json.Unmarshal(bdRespData, bdResp)
+	if err != nil {
+		log.Infof("bd err: %v", err)
+		return nil, err
+	}
+	return bdResp, nil
+}
---
+++ baidu/face_set_user_add.go
@@ -0,0 +1,67 @@
+package baidu
+
+import (
+	"encoding/base64"
+	"github.com/brickman-source/golang-utilities/http"
+	"github.com/brickman-source/golang-utilities/json"
+	"github.com/brickman-source/golang-utilities/log"
+	"net/url"
+)
+
+type FaceSetUserAddRequest struct {
+	Image           string `json:"image"`
+	ImageType       string `json:"image_type"`
+	GroupID         string `json:"group_id"`
+	UserID          string `json:"user_id"`
+	UserInfo        string `json:"user_info"`
+	QualityControl  string `json:"quality_control"`
+	LivenessControl string `json:"liveness_control"`
+}
+type FaceSetUserAddResponse struct {
+	LogID     uint64 `json:"log_id"`
+	FaceToken string `json:"face_token"`
+	Location  struct {
+		Left     int `json:"left"`
+		Top      int `json:"top"`
+		Width    int `json:"width"`
+		Height   int `json:"height"`
+		Rotation int `json:"rotation"`
+	} `json:"location"`
+}
+
+func (bd *Baidu) FaceSetUserAdd(userGroupId, userId string, userInfo string, imageData []byte, appId, appSecret string) (*FaceSetUserAddResponse, error) {
+	accessToken, err := bd.GetAccessTokenByClient(appId, appSecret)
+	if err != nil {
+		log.Errorf("cannot get access token(%v): %v", appId, err.Error())
+		return nil, err
+	}
+	bdReqURL, _ := url.Parse(`https://aip.baidubce.com/rest/2.0/face/v3/faceset/user/add`)
+	bdReqQuery := bdReqURL.Query()
+	bdReqQuery.Set("access_token", accessToken.AccessToken)
+	bdReqURL.RawQuery = bdReqQuery.Encode()
+
+	req := &FaceSetUserAddRequest{
+		Image:     base64.StdEncoding.EncodeToString(imageData),
+		ImageType: "BASE64",
+		GroupID:   userGroupId,
+		UserID:    userId,
+		UserInfo:  userInfo,
+	}
+
+	bdRespData, err := http.PostData(bdReqURL.String(),
+		http.MIMEApplicationJSONCharsetUTF8,
+		json.ShouldMarshal(req),
+	)
+
+	if err != nil {
+		log.Infof("bd err: %v", err)
+		return nil, err
+	}
+	bdResp := &FaceSetUserAddResponse{}
+	err = json.Unmarshal(bdRespData, bdResp)
+	if err != nil {
+		log.Infof("bd err: %v", err)
+		return nil, err
+	}
+	return bdResp, nil
+}
